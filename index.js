11ce15e2231cf68e2fe9a3798ed80b16e7c7757c
trustytrojan <tarbot24@hotmail.com>
2024-01-08 13:20:07 -0500

    Fix path handling in index.js

--- index.js
+++ index.js
@@ -16,11 +16,15 @@ app.get("*", (req, res) => {
 	/** @type {string} */
 	// remove duplicate slashes between path components
 	const path = req.url.replaceAll(/\/\/+/g, "/");
+	console.log(path);
 	const secondSlash = path.indexOf("/", 1);
-	const firstComponent = path.substring(0, secondSlash);
-	const rest = path.substring(secondSlash);
+	const firstComponent = path.substring(1, secondSlash);
+	console.log(firstComponent);
+	let rest = path.substring(secondSlash);
+	if (!rest) rest = "/";
+	console.log(rest);
 	const port = PORT_MAP[firstComponent];
-	console.log(firstComponent, port);
+	console.log(port);
 	if (!port)
 		res.sendFile("index.html");
 	else
