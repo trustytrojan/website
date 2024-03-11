1f326679949aa97bf3091d2970b4d7474d561f21
trustytrojan <tarbot24@hotmail.com>
2024-03-10 23:53:14 -0400

    forget drag&drop, this works and looks better

--- res/js/ytdl/elements.js
+++ res/js/ytdl/elements.js
@@ -1,65 +1,19 @@
-let cloneCount = 0;
-
-/**
- * @param {HTMLTableSectionElement} el 
- */
-const cloneTableRow = (el) => {
-	/** @type {HTMLTableSectionElement} */
-	const clone = el.cloneNode(true);
-	clone.id = `${el.id}-${cloneCount++}`;
-	clone.ondragstart = (ev) => ev.dataTransfer.setData('id', clone.id);
-	return clone;
-};
-
-/** @type {HTMLDivElement} */
-export const videoFormatDrop = document.getElementById('video-format-drop');
-videoFormatDrop.ondragover = (ev) => ev.preventDefault();
-videoFormatDrop.ondrop = (ev) => {
-	ev.preventDefault();
-	if (videoFormatDrop.childElementCount)
-		return;
-	const el = document.getElementById(ev.dataTransfer.getData('id'));
-	if (el.dataset.type === 'audio')
-		return alert('this is not a video format!');
-	videoFormatDrop.appendChild(cloneTableRow(el));
-	dlButton.hidden = false;
-};
-
-/** @type {HTMLDivElement} */
-export const audioFormatDrop = document.getElementById('audio-format-drop');
-audioFormatDrop.ondragover = (ev) => ev.preventDefault();
-audioFormatDrop.ondrop = (ev) => {
-	ev.preventDefault();
-	if (audioFormatDrop.childElementCount)
-		return;
-	const el = document.getElementById(ev.dataTransfer.getData('id'));
-	if (el.dataset.type !== 'audio')
-		return alert('this is not an audio format!');
-	audioFormatDrop.appendChild(cloneTableRow(el));
-	dlButton.hidden = false;
-};
-
-// delete a row if it wasnt dragged from the table
-document.ondragover = (ev) => ev.preventDefault();
-document.ondrop = (ev) => {
-	ev.preventDefault();
-	const el = document.getElementById(ev.dataTransfer.getData('id'));
-	if (el.parentElement === tableBody) return;
-	el.remove();
-	if (!audioFormatDrop.childElementCount && !videoFormatDrop.childElementCount)
-		dlButton.hidden = true;
-};
-
-/** @type {HTMLButtonElement} */
-export const dlButton = document.getElementById('dl-button');
+/** @type {HTMLTableSectionElement} */
+export const videoTb = document.getElementById('video-tb');
 
 /** @type {HTMLTableSectionElement} */
-export const tableBody = document.getElementById('table-body');
+export const audioTb = document.getElementById('audio-tb');
+
+/** @type {HTMLTableSectionElement} */
+export const avTb = document.getElementById('av-tb');
 
 /** @type {HTMLInputElement} */
 export const ytIdOrUrl = document.getElementById('yt-id-or-url');
 
 export const details = Object.freeze({
+	/** @type {HTMLAnchorElement} */
+	root: document.getElementById('details-root'),
+
 	/** @type {HTMLImageElement} */
 	thumbnail: document.getElementById('details-thumbnail'),
 	
@@ -70,7 +24,7 @@ export const details = Object.freeze({
 	
 	/** @type {HTMLDivElement} */
 	viewsLength: document.getElementById('details-views-length'),
-	
-	/** @type {HTMLDivElement} */
-	id: document.getElementById('details-id')
 });
+
+/** @type {HTMLAnchorElement} */
+export const dlButton = document.getElementById('dl-button');
--- res/js/ytdl/ytdl.js
+++ res/js/ytdl/ytdl.js
@@ -1,63 +1,117 @@
 import { createElement } from '../utils/elements.js';
+import { max } from '../utils/arrays.js';
 import * as els from './elements.js';
 
-const typeStr = (format) =>
-	(format.hasVideo && format.hasAudio)
-		? 'a/v'
+let videoId;
+
+const extendFormat = (format) => {
+	const bitrateKbps = format.hasVideo
+		? Math.round((format.averageBitrate || format.bitrate) / 1000)
+		: format.audioBitrate;
+	format.bitrateKbpsText = `${bitrateKbps}kbps`;
+	format.type = (format.hasVideo && format.hasAudio)
+		? 'av'
 		: (format.hasVideo ? 'video' : 'audio');
+};
 
-const getBitrateKbps = (format) => format.hasVideo ? Math.round((format.averageBitrate || format.bitrate) / 1000) : format.audioBitrate;
+/** @type {Record<string, HTMLDivElement?>} */
+const selectedFormat = Object.preventExtensions({ audio: null, video: null, av: null });
 
-const createTableRow = (format, details) => {
-	const id = `${details.videoId}-${format.itag}`;
+const updateDlButton = () => {
+	const { audio, video, av } = selectedFormat;
+	if (av) {
+		els.dlButton.classList.remove('dl-btn-disabled');
+		els.dlButton.href = `${baseUrl}/yt/dl/${videoId}?itags=${av.id.split('-')[1]}`;
+	} else if (audio || video) {
+		els.dlButton.classList.remove('dl-btn-disabled');
+		if (audio && video)
+			els.dlButton.href = `${baseUrl}/yt/dl/${videoId}?itags=${audio.id.split('-')[1]},${video.id.split('-')[1]}`;
+		else if (audio)
+			els.dlButton.href = `${baseUrl}/yt/dl/${videoId}?itags=${audio.id.split('-')[1]}`;
+		else if (video)
+			els.dlButton.href = `${baseUrl}/yt/dl/${videoId}?itags=${video.id.split('-')[1]}`;
+	} else {
+		els.dlButton.classList.add('dl-btn-disabled');
+		els.dlButton.href = '';
+	}
+};
+
+const createTableRow = (
+	{ itag, type, codecs, hasVideo, bitrateKbpsText, qualityLabel, audioQuality },
+	{ videoId }
+) => {
 	const el = createElement('tr', {
-		draggable: true, id,
-		dataset: {
-			type: typeStr(format),
-			itag: format.itag
+		className: 'tt-hover',
+		id: `${videoId}-${itag}`,
+		onclick: () => {
+			// get references to currently selected formats
+			let { audio, video, av } = selectedFormat;
+
+			if (selectedFormat[type] === el) {
+				// if this format is already selected, de-select it
+				el.classList.remove('selected-format');
+				selectedFormat[type] = null;
+			} else {
+				// keep mutual exclusivity
+				if (type === 'av') {
+					audio?.classList.remove('selected-format');
+					video?.classList.remove('selected-format');
+					selectedFormat.audio = null;
+					selectedFormat.video = null;
+				} else {
+					av?.classList.remove('selected-format');
+					selectedFormat.av = null;
+				}
+
+				// select self, deselect other
+				el.classList.add('selected-format');
+				selectedFormat[type]?.classList.remove('selected-format');
+				selectedFormat[type] = el;
+			}
+
+			updateDlButton();
 		},
-		ondragstart: (ev) => ev.dataTransfer.setData('id', id)
 	}, [
-		createElement('td', { innerHTML: format.itag }),
-		createElement('td', { innerHTML: typeStr(format) }),
-		createElement('td', { innerHTML: format.hasVideo ? format.qualityLabel : format.audioQuality.split('_')[2].toLowerCase() }),
-		createElement('td', { innerHTML: format.codecs }),
-		createElement('td', { innerHTML: `${getBitrateKbps(format)}kbps` })
+		createElement('td', { innerHTML: itag }),
+		createElement('td', { innerHTML: hasVideo ? qualityLabel : audioQuality.split('_')[2].toLowerCase() }),
+		createElement('td', { innerHTML: codecs }),
+		createElement('td', { innerHTML: bitrateKbpsText })
 	]);
 	return el;
-}
+};
 
-const baseUrl = `https://api.trustytrojan.dev`;
-let _details, videoId;
+const baseUrl = 'https://api.trustytrojan.dev';
 
 export const getInfo = async () => {
 	const idOrUrl = els.ytIdOrUrl.value;
 	if (!idOrUrl.length) return;
+
+	els.videoTb.replaceChildren();
+	els.audioTb.replaceChildren();
+	selectedFormat.av = null;
+
 	const url = `${baseUrl}/yt/info/${encodeURIComponent(idOrUrl)}`;
 	const { formats, details } = await (await fetch(url)).json();
+
+	({ videoId } = details);
+	els.details.root.href = `https://youtu.be/${videoId}`;
+	els.details.thumbnail.src = details.thumbnails[0].url;
 	els.details.title.innerHTML = details.title;
 	els.details.channel.innerHTML = details.ownerChannelName;
 	els.details.viewsLength.innerHTML = `${details.viewCount} views • ${Math.floor(details.lengthSeconds / 60)}:${details.lengthSeconds % 60}`;
-	els.details.id.innerHTML = videoId = details.videoId;
-	els.details.thumbnail.src = details.thumbnails[0].url;
-	for (const format of formats)
-		els.tableBody.append(createTableRow(format, details));
-	document.getElementById('everything-else').style.display = 'flex';
-	_details = {
-		title: details.title,
-		ownerChannelName: details.ownerChannelName,
-		publishDate: details.publishDate,
-		lengthSeconds: details.lengthSeconds
-	};
-};
 
-export const ffmpegDl = async () => {
-	const itags = [
-		els.videoFormatDrop.firstElementChild?.dataset.itag,
-		els.audioFormatDrop.firstElementChild?.dataset.itag
-	].filter(x => x).join(',');
-	createElement('a', {
-		href: `${baseUrl}/yt/dl/${videoId}?itags=${itags}`,
-		// download: `${_details.title} - ${_details.ownerChannelName}.mkv`
-	}).click();
+	for (const format of formats) {
+		extendFormat(format);
+		els[format.type + 'Tb'].append(createTableRow(format, details));
+	}
+
+	const highestVideo = document.getElementById(`${videoId}-${max(formats, format => format.bitrate).itag}`);
+	const highestAudio = document.getElementById(`${videoId}-${max(formats, format => format.audioBitrate).itag}`);
+	highestVideo.classList.add('selected-format');
+	highestAudio.classList.add('selected-format');
+	selectedFormat.audio = highestAudio;
+	selectedFormat.video = highestVideo;
+	updateDlButton();
+
+	document.getElementById('everything-else').style.display = 'flex';
 };
