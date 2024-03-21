cc374fcd8df5407d2074c9e47c98661140af65ef
trustytrojan <tarbot24@hotmail.com>
2024-03-21 15:01:15 +0000

    now is the last time

--- res/js/proxy.js
+++
@@ -1,9 +0,0 @@
-const baseUrl = 'https://proxy.trustytrojan.dev/?uri=';
-
-export const wrapUrl = (url) => `${baseUrl}${encodeURIComponent(url)}`;
-
-/**
- * @param {string} url 
- * @param {RequestInit} [init] 
- */
-export default (url, init) => fetch(wrapUrl(url), init);
